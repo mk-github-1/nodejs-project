8ffe4246163b89dbd92f92262fd2e2cf8ca6d55a
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2023-06-21 08:53:23 +0900

    Update loginUser.repository.ts

--- back-end/src/providers/domain-service/repository/loginUser.repository.ts
+++ back-end/src/providers/domain-service/repository/loginUser.repository.ts
@@ -1,11 +1,9 @@
-/*
 import { LoginUserModel } from '@/providers/domain-model/model/LoginUserModel';
 
 export interface LoginUserRepository {
     findAll(): Promise<LoginUserModel[]>;
     findById(id: string): Promise<LoginUserModel>;
-    create(model: LoginUserModel): Promise<LoginUserModel>;
-    update(id: string, model: LoginUserModel): Promise<LoginUserModel>;
+    create(loginUserModel: LoginUserModel): Promise<LoginUserModel>;
+    update(id: string, loginUserModel: LoginUserModel): Promise<LoginUserModel>;
     delete(id: string): Promise<void>;
 }
- */
