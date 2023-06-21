c28fd22d5c363547630a083629736d4f6cd69f69
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2023-06-21 12:28:59 +0900

    Delete back-end/src/providers/domain-service/repository directory

--- back-end/src/providers/domain-service/repository/loginUser.repository.ts
+++
@@ -1,9 +0,0 @@
-import { LoginUserModel } from '@/providers/domain-model/model/LoginUserModel';
-
-export interface LoginUserRepository {
-    findAll(): Promise<LoginUserModel[]>;
-    findById(id: string): Promise<LoginUserModel>;
-    create(loginUserModel: LoginUserModel): Promise<LoginUserModel>;
-    update(id: string, loginUserModel: LoginUserModel): Promise<LoginUserModel>;
-    delete(id: string): Promise<void>;
-}
