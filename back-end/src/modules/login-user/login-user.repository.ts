bc5dbab05e6f92944f8e4fb4f85fc1e5ba0cb28a
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2023-06-23 19:49:49 +0900

    Repository作成

--- back-end/src/modules/login-user/login-user.repository.ts
+++ back-end/src/modules/login-user/login-user.repository.ts
@@ -1,50 +1,88 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
 import { Repository } from 'typeorm';
-import { classToPlain, plainToClass } from 'class-transformer';
 
 import { LoginUser } from '@/providers/domain-model/entity/LoginUser';
-import { LoginUserRole } from '@/providers/domain-model/entity/LoginUserRole';
 import { LoginUserModel } from '@/providers/domain-model/model/LoginUserModel';
 import { LoginUserRoleModel } from '@/providers/domain-model/model/LoginUserRoleModel';
-import { Injectable } from '@nestjs/common';
+import { IGenericRepository } from '@/providers/IGenericRepository';
 
-// @Injectable
-export class LoginUserRepository extends Repository<LoginUser> {
-    /*
-    async findAll(): Promise<LoginUserRoleModel[]> {
-        const loginUsers: LoginUser[] = await this.find();
-        const loginUserRoles: LoginUserRoleModel[] = loginUsers.map((e) => {
-            const { roles, ...userData } = e;
-            const roleModels: LoginUserRoleModel[] = roles.map((role) => {
-                // roleをLoginUserRoleModelに変換する処理を実行する
-                // 例: const roleModel: LoginUserRoleModel = plainToClass(LoginUserRoleModel, role);
-                //     return roleModel;
-            });
-            return {
-                ...userData,
-                roles: roleModels,
-            };
-        });
-        return userRoles;
+/*
+ * Interface (CRUD + sort)
+ */
+export interface ILoginUserRepository extends IGenericRepository<LoginUserModel> {
+    findAll(): Promise<LoginUserModel[]>;
+    findById(account: string): Promise<LoginUserModel>;
+    create(loginUserModel: LoginUserModel): Promise<void>;
+    update(account: string, loginUserModel: LoginUserModel): Promise<void>;
+    delete(account: string): Promise<void>;
+    sort(sortLists: Record<string, number>[]): Promise<void>;
+}
+
+/*
+ * Class
+ */
+@Injectable()
+export class LoginUserRepository implements ILoginUserRepository {
+    constructor(
+        @InjectRepository(LoginUser)
+        private loginUserRepository: Repository<LoginUser>,
+    ) {}
+
+    async findAll(): Promise<LoginUserModel[]> {
+        // データ操作 → Entity to Model マッピング
+        const loginUsers: LoginUser[] = await this.loginUserRepository.find();
+
+        const loginUsersModel: LoginUserModel[] = [];
+
+        for (const loginUser of loginUsers) {
+            const loginUserRoleModels: LoginUserRoleModel[] = loginUser.loginUserRoles.map((role) =>
+                plainToClass(LoginUserRoleModel, role),
+            );
+            const loginUserModel: LoginUserModel = plainToClass(LoginUserModel, loginUser);
+            loginUserModel.loginUserRoleModels = loginUserRoleModels;
+            loginUsersModel.push(loginUserModel);
+        }
+
+        return loginUsersModel;
     }
-     */
 
-    // id: number -> account: string
-    /*
+    // 通常は id: number
     async findById(account: string): Promise<LoginUserModel> {
-        const user: LoginUser = await this.findOne({ account });
-        return plainToClass(LoginUserModel, user);
-    }
-     */
+        // データ操作 → Entity to Model マッピング
+        const loginUser: LoginUser = await this.loginUserRepository.findOne({ where: { account } });
 
-    async createLoginUser(loginUserModel: LoginUserModel): Promise<void> {
-        const loginUser: LoginUser = this.create(loginUserModel);
+        const userRoles: LoginUserRoleModel[] = loginUser.loginUserRoles.map((role) =>
+            plainToClass(LoginUserRoleModel, role),
+        );
+        const loginUserModel: LoginUserModel = plainToClass(LoginUserModel, loginUser);
+        loginUserModel.loginUserRoleModels = userRoles;
+
+        return loginUserModel;
     }
 
-    async updateLoginUser(account: string, loginUserModel: LoginUserModel): Promise<void> {
-        await this.update(account, loginUserModel);
+    async create(loginUserModel: LoginUserModel): Promise<void> {
+        // Model to Entity マッピング → データ操作
+        const loginUser: LoginUser = plainToClass(LoginUser, loginUserModel);
+        await this.loginUserRepository.create(loginUser);
     }
 
-    async deleteLoginUser(account: string): Promise<void> {
-        await this.delete(account);
+    async update(account: string, loginUserModel: LoginUserModel): Promise<void> {
+        // Model to Entity マッピング → データ操作
+        const loginUser: LoginUser = plainToClass(LoginUser, loginUserModel);
+        await this.loginUserRepository.update(account, loginUserModel);
+    }
+
+    async delete(account: string): Promise<void> {
+        // データ操作
+        await this.loginUserRepository.delete(account);
+    }
+
+    // 通常はRecord<number, number>[]
+    async sort(sortLists: Record<string, number>[]): Promise<void> {
+        // ソート処理のSQLを実装する
+        const query = 'SELECT * FROM your_table';
+        const result = await this.loginUserRepository.query(query);
     }
 }
