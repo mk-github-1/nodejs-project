09e5b9cd069520337f65aa42d3a99ac64017f8be
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2023-06-27 07:21:07 +0900

    修正

--- front-end/src/pages/LoginUser/LoginUser.tsx
+++ front-end/src/pages/LoginUser/LoginUser.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const LoginUser = () => {
+    // コンポーネントがマウントされた時に実行されるコード
+    // 第二引数の空配列[]は、依存関係がないことを示し、マウント時のみ実行することを意味します
+    useEffect(() => {
+        console.log("コンポーネントがマウントされました");
+
+        // ここに任意のアクションを追加します
+    }, []);
+
     return (
         <>
             LoginUserコンポーネントです。 <br />
---
+++ front-end/src/pages/LoginUser/LoginUserEdit/loginUserEdit.tsx
@@ -0,0 +1,3 @@
+const loginUserLogic = () => {};
+
+export default loginUserLogic;
