1fb4aeb76d5ca42ce101f027675b2e929b9c6cdb
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2023-07-08 17:01:47 +0900

    修正

--- back-end/src/providers/domain-model/model/LoginUserRoleModel.ts
+++ back-end/src/providers/domain-model/model/LoginUserRoleModel.ts
@@ -4,7 +4,7 @@ import { LoginUserModel } from '@/providers/domain-model/model/LoginUserModel';
 import { RoleModel } from '@/providers/domain-model/model/RoleModel';
 
 export class LoginUserRoleModel {
-  public constructor(options?: { account?: string; roleId?: string; sortOrder?: number; isDeleted?: boolean; createdAt?: Date; updatedAt?: Date; timestamp?: number; loginUserModel?: LoginUserModel; roleModel?: RoleModel }) {
+  public constructor(options?: { account?: string; roleId?: string; sortOrder?: number; isDeleted?: boolean; createdAt?: Date; updatedAt?: Date; timestamp?: number; roleModel?: RoleModel }) {
     this.account = options?.account || '';
     this.roleId = options?.roleId || '';
     this.sortOrder = options?.sortOrder || 0;
@@ -12,7 +12,6 @@ export class LoginUserRoleModel {
     this.createdAt = options?.createdAt || new Date();
     this.updatedAt = options?.updatedAt || new Date();
     this.timestamp = options?.timestamp || 0;
-    this.loginUserModel = options?.loginUserModel || new LoginUserModel();
     this.roleModel = options?.roleModel || new RoleModel();
   }
 
@@ -36,8 +35,6 @@ export class LoginUserRoleModel {
 
   @Expose()
   public timestamp: number;
-
-  public loginUserModel: LoginUserModel;
 
   @Expose()
   public roleModel: RoleModel;
